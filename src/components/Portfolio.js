818d4b152ae8551de2af4b58d56908b68980cf0e
Ben Martin <bmartin2009@gmail.com>
2022-10-04 21:52:46 -0500

    Added new field to projects to render message about deployment.

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -12,10 +12,11 @@ function Portfolio() {
     {
       name: 'Dungeon Delver (WIP, development stage)',
       link: 'https://dungeon-delver-main.herokuapp.com/',
-      description: 'One of the toughtest parts of running a tabletop role-playing game is simply keeping track of every last creature, trinket, and secret. Dungeon Delver is a full-stack MERN website that makes tracking a sprawling campaign as easy as clicking a button. This application takes the tedium out of running campaigns by letting users quickly and easily do things like create dungeons and rooms, manage monsters, update stats, across any number of campaigns. (Refactored from a previous version using Handlebars and Sequelize.)',
+      description: 'One of the toughtest parts of running a tabletop role-playing game is simply keeping track of every last creature, trinket, and secret. Dungeon Delver is a full-stack MERN website that makes tracking a sprawling campaign as easy as clicking a button. This application takes the tedium out of running campaigns by letting users quickly and easily do things like create dungeons and rooms, manage monsters, update stats, across any number of campaigns.',
       image: imgDelver,
       alt: 'Dungeon Delver, for all you campaign organizational needs!',
       repo: 'https://github.com/the-wake/dungeon-delver-main',
+      deployment: 'heroku',
     },
     {
       name: 'Jarvis: The Smash Bros. Companion (WIP, MVP)',
@@ -24,6 +25,7 @@ function Portfolio() {
       image: imgJarvis,
       alt: 'Jarvis helps players track their Super Smash Bros. challenge attempts.',
       repo: 'https://github.com/the-wake/jarvis-smash-companion',
+      deployment: 'github-pages',
     },
     {
       name: 'Dungeon Seekers',
@@ -32,6 +34,7 @@ function Portfolio() {
       image: imgSeekers,
       alt: 'Dungeon Seekers makes finding the right creature a breeze!',
       repo: 'https://github.com/the-wake/dungeon-seekers',
+      deployment: 'github-pages',
     },
     {
       name: 'Full-Stack Blog',
@@ -40,22 +43,25 @@ function Portfolio() {
       image: imgBlog,
       alt: 'This is one of the blogs of all time.',
       repo: 'https://github.com/the-wake/assignment-14-blog',
+      deployment: 'heroku',
     },
     {
       name: 'Employee Management System',
       link: 'https://github.com/the-wake/assignment-12-employee-management',
-      description: 'A fully back-end application run via the command line (Node.js). Users can create and update departments, job titles, and employees. The back-end MySQL database enables useful and powerful connections to be created between data points for easy reference and correlation. (Back-end only; no live deployment.)',
+      description: 'A fully back-end application run via the command line (Node.js). Users can create and update departments, job titles, and employees. The back-end MySQL database enables useful and powerful connections to be created between data points for easy reference and correlation.',
       image: imgEms,
       alt: 'Create and querry teams in a flash!',
       repo: 'https://github.com/the-wake/assignment-12-employee-management',
+      deployment: 'backend-only',
     },
     {
       name: 'Social Media API',
       link: 'https://github.com/the-wake/a18-social-media-api',
-      description: 'This back-end Mongoose-based API has a robust suite of queries allowing users to create accounts, post thoughts, and comment on each other\'s posts. The NoSQL nature of the database allows for greater flexibility in the data, and allows easy creation of connections between data points. (Back-end only; no live deployment.)',
+      description: 'This back-end Mongoose-based API has a robust suite of queries allowing users to create accounts, post thoughts, and comment on each other\'s posts. The NoSQL nature of the database allows for greater flexibility in the data, and allows easy creation of connections between data points.',
       image: imgSocialMedia,
       alt: 'Putting the API back in APprecIation!',
       repo: 'https://github.com/the-wake/a18-social-media-api',
+      deployment: 'backend-only',
     },
   ]);
 
--- src/components/Project.js
+++ src/components/Project.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Project({ project }) {
-  const { name, link, description, repo, image, alt } = project;
+  const { name, link, description, repo, image, alt, deployment } = project;
+
+  const genMessage = () => {
+    if (deployment === 'github-pages') {
+      return `Deployed on Github Pages.`
+    } else if (deployment === 'heroku') {
+      return `Deployed on Heroku – Expect delay on load.`
+    } else if (deployment === 'backend-only') {
+      return `Backend only – See GitHub Repo for readme and demonstration.`
+    };
+  };
+
+  const deployMessage = genMessage();
 
   return (
     <div className="project-container">
@@ -11,6 +23,7 @@ function Project({ project }) {
       <div className="content-container">
         <a href={link} target="_blank" rel="noreferrer"><h3>{name}</h3></a>
         <p>{description}</p>
+        <p>{deployMessage}</p>
         <a href={repo} target="_blank" rel="noreferrer" className="github-element">GitHub Repo</a>
       </div>
     </div>
